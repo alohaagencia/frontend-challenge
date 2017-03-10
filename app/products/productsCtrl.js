04a9e9aeec18eb5db05ded901cbe2e99007bf660
Maycon <mayconds000@gmail.com>
2017-03-10 10:05:53 -0300

    correcoes e filtro para atualizar o valor total de acordo com que a lista for filtrada

--- app/products/productsCtrl.js
+++ app/products/productsCtrl.js
@@ -1,19 +1,43 @@
 (function() {
-  angular.module('myApp').controller('productsCtrl', [
+ const app = angular.module('myApp');
+ 
+ app.controller('productsCtrl', [
     'tabs',
-    '$http', 
+    '$http',
+    '$scope', 
     productsController
-  ])
+  ]);
+  
+  app.filter('getTotalList', function() {
+     return function(input) {
+      input = input || [];
+      let arr = [];
+      let total = 0;
 
-  function productsController(tabs, $http) {
+      for(let i in input) {
+        total += input[i].price;
+        arr.push(input[i]);
+      }
+
+      arr.push(total);
+      return arr;
+    }
+  });
+
+   
+
+
+  function productsController(tabs, $http, $scope) {
     //garantido o scopo corretamente dentro do controller e das funções
     const vm = this;
     let idProduct = 3;
     vm.msgSuccess = false;
+    const toggleMsgSuccess = function() {
+      vm.msgSuccess = vm.msgSuccess === false ? true : false;
+    };
 
     $http.get('./database.json').then(function(result) {
           vm.list = result.data;
-          vm.calculateValue();
     });
 
     vm.create = function() {
@@ -21,11 +45,15 @@
       vm.product._id = idProduct;
       vm.product.created_at = new Date();
       vm.list.push(vm.product);
-      vm.refresh();
       vm.msgSuccess = true;
-      setTimeout(function(){
-        vm.msgSuccess = false;
-      }, 5000);
+      vm.refresh();
+       setTimeout(function(){
+        toggleMsgSuccess();
+        $scope.$digest();
+      }, 10000);
+     
+     
+      
     };
 
     vm.update = function() {
@@ -40,7 +68,6 @@
 
     vm.refresh = function() {
       vm.product = {};
-      vm.calculateValue();
       tabs.show(vm, {tabList: true, tabCreate: true});
     }
 
@@ -54,17 +81,6 @@
       vm.product = product;
       vm.index = index;
       tabs.show(vm, {tabDelete: true});
-    }
-
-    vm.calculateValue = function() {
-      let total = 0;
-      let products = vm.list;
-      
-      for(let i in products) {
-        total += products[i].price;
-      }
-
-      vm.total = total;
     }
 
     vm.refresh();
